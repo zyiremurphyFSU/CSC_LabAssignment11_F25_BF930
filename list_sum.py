9e6c3cf15d6fd3643ac90ce5462ec6ea9003bd5e
Zyire <zmurphy5@broncos.uncfsu.edu>
2025-10-31 10:18:45 -0400

    list_sum.py finish

--- list_sum.py
+++ list_sum.py
@@ -7,13 +7,14 @@ def sum_list(numbers):
     @return int: Sum of the list elements.
     """
 
-    # TODO: set a running total variable
+    total = 0
+      
+    for num in numbers:
+        total += num
 
-    # TODO: use loop to calculate the sum of all numbers in the list
-
-    # TODO: return the sum
+    return total
 
 
 if __name__ == "__main__":
     nums = [5, 19, 2, 33, -5, 2]
-    # TODO: call function sum_list() and send argument 'nums', then print it out for your testing
+    print(sum_list(nums))
